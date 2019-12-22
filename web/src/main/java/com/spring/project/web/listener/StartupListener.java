fb7f6abc644ad2f31a3cfbed57bde57d9c1ecb48
buitahau@gmail.com <buitahau@gmail.com>
2019-12-22 15:03:41 +0700

    Get configuration from file properties

--- web/src/main/java/com/spring/project/web/listener/StartupListener.java
+++ web/src/main/java/com/spring/project/web/listener/StartupListener.java
@@ -5,9 +5,11 @@ import com.spring.project.common.dto.UserDTO;
 import com.spring.project.common.enums.StatusEnum;
 import com.spring.project.manager.UserManager;
 import com.spring.project.web.constants.Constants;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,35 +28,63 @@ public class StartupListener implements InitializingBean {
 		initData();
 	}
 
+	private UserDTO createUserDTOFromUsernameAndPassword(
+					String userName, String password) {
+
+		UserDTO userDTO = new UserDTO();
+		userDTO.setStatus(StatusEnum.ACTIVE.getValue());
+		userDTO.setUserName(userName);
+		userDTO.setPassword(password);
+		return userDTO;
+	}
+
 	/**
 	 * Count users, if = 0, create default admin user
 	 */
 	private void initData() {
 
 		int totalUsers = userManager.countAll();
-		logger.info("Init Sample Data");
 
-		if (totalUsers == 0) {
-			RoleDTO roleDTO = new RoleDTO();
-			roleDTO.setRole(Constants.ROLE_ADMIN_DEFAULT);
-			List<RoleDTO> roles = new ArrayList<>();
-			roles.add(roleDTO);
+		if (totalUsers > 0 || StringUtils.isBlank(DEFAULT_ADMIN_USERNAME) ||
+						StringUtils.isBlank(DEFAULT_ADMIN_PASSWORD) ||
+						StringUtils.isBlank(DEFAULT_USER_USERNAME) ||
+						StringUtils.isBlank(DEFAULT_USER_PASSWORD)) {
 
-			UserDTO userDTO = new UserDTO();
-			userDTO.setStatus(StatusEnum.ACTIVE.getValue());
-			userDTO.setPassword(DEFAULT_ADMIN_PASSWORD);
-			userDTO.setUserName(DEFAULT_ADMIN_USERNAME);
-			userDTO.setRoles(roles);
-			userManager.addUser(userDTO);
+			return;
 		}
+
+		logger.info("Begin initializing the sample data...");
+		RoleDTO roleDTO = new RoleDTO();
+		roleDTO.setRole(Constants.ROLE_ADMIN_DEFAULT);
+		List<RoleDTO> roles = new ArrayList<>();
+		roles.add(roleDTO);
+
+		UserDTO userAdminDTO = createUserDTOFromUsernameAndPassword(
+						DEFAULT_ADMIN_USERNAME, DEFAULT_ADMIN_PASSWORD);
+		userAdminDTO.setRoles(roles);
+		userManager.addUser(userAdminDTO);
+
+		UserDTO userDTO = createUserDTOFromUsernameAndPassword(
+						DEFAULT_USER_USERNAME, DEFAULT_USER_PASSWORD);
+		userManager.addUser(userDTO);
+
+		logger.info("...End initializing the sample data!");
 	}
 
 	@Autowired
 	private UserManager userManager;
 
-	private static final String DEFAULT_ADMIN_USERNAME = "admin";
+	@Value("${default.admin.username}")
+	private String DEFAULT_ADMIN_USERNAME;
 
-	private static final String DEFAULT_ADMIN_PASSWORD = "123456";
+	@Value("${default.admin.password}")
+	private String DEFAULT_ADMIN_PASSWORD;
+
+	@Value("${default.user.username}")
+	private String DEFAULT_USER_USERNAME;
+
+	@Value("${default.user.password}")
+	private String DEFAULT_USER_PASSWORD;
 
 	private static final Logger logger =
 					Logger.getLogger(StartupListener.class);
